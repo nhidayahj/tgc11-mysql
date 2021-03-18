c1004effe45440507f70a51da19752ebe9c356b8
nhidayahj <hidayah.joha@gmail.com>
2021-03-18 06:30:15 +0000

    added hbs file, create express connection and sending in first query

--- sql-express/README.md
+++ sql-express/README.md
@@ -7,3 +7,8 @@ yarn add wax-on
 yarn add mysql2
 yarn add handlebars-helpers
 ```
+
+```
+- learn technique on array and object destructuring 
+- useful when doing mysql with express
+```
--- sql-express/index.js
+++ sql-express/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const hbs = require("hbs");
 const wax = require("wax-on");
+
+// the '/promise ==> allow us to use the await/async 
 const mysql = require("mysql2/promise");
 
 let app = express();
@@ -10,6 +12,39 @@ wax.on(hbs.handlebars);
 wax.setLayoutPath("./views/layouts");
 
 app.use(express.urlencoded({ extended: false }));
+
+async function main() {
+    // create sql connection
+    const connection = await mysql.createConnection({
+        // host is usually  
+        'host':'localhost',
+        'user':'root',
+        'database':'sakila'
+
+    });
+
+    // let query = "SELECT * FROM actors";
+    // // the syntax below is known as ARRAY DESTRUCTURING
+    // let [actors] = await connection.execute("SELECT * FROM actor");
+    // // instead of
+    // // let response = await connection.execute("SELECT * FROM actor");
+    // // let actors = response[0];
+    // // for (let a of actors) {
+    // //     console.log(a);
+    // // }
+    // console.log(actors.toJSON())
+
+    let query = 'SELECT * from actor';
+    app.get('/actors', async (req,res) => {
+        let [actors] = await connection.execute(query);
+        res.render('actors', {
+            'actors':actors
+        })
+    })
+
+}
+
+main()
 
 require("handlebars-helpers")({
   handlebars: hbs.handlebars,
